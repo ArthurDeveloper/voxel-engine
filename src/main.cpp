92b6c45bf759347dcfafa61fbbbccee4902bc8f0
ArthurDev <arthurdevoficial@gmail.com>
2023-01-25 21:33:33 -0300

    feat: Added the ability to look around with the camera

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@
 #include <fstream>
 #include <glm/ext/matrix_float4x4.hpp>
 #include "glm/ext/matrix_clip_space.hpp"
+#include <glm/vec3.hpp>
 #include "shader.h"
 #include "VAO.h"
 #include "VBO.h"
@@ -18,11 +19,24 @@
 #define WINDOW_WIDTH 640
 #define WINDOW_HEIGHT 480
 
+float pitch = 0.0f;
+float yaw = 0.0f;
+
+glm::vec3 direction;
+glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, 0.0f);
+glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
+
 glm::vec3 viewPosition = glm::vec3(0.0f, 0.0f, -3.0f);
 
+// Relative to mouse
+GLfloat lastX = (GLfloat)WINDOW_WIDTH/2, lastY = (GLfloat)WINDOW_HEIGHT/2;
+GLfloat sensitivity = 0.1f;
+
 void handleInputs(GLFWwindow *window);
+void mouseCallback(GLFWwindow *window, GLdouble ax, GLdouble ay);
 
 int main(void) {
+
 	GLfloat vertices[] = {
 		// Coordinates		    // Colors            // Texture
 		 0.5f,    0.5f, 0.0f,   1.0f, 0.0f, 0.0f,    1.0f, 1.0f,
@@ -88,6 +102,9 @@ int main(void) {
 	texture.bind();
 	texture.params();
 	texture.build();
+
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+	glfwSetCursorPosCallback(window, mouseCallback);
 	
 	while (!glfwWindowShouldClose(window)) {
 		handleInputs(window);
@@ -101,7 +118,7 @@ int main(void) {
 		shader.use();
 
 		glm::mat4 view = glm::mat4(1.0f);
-		view = glm::translate(view, viewPosition);
+		view = glm::lookAt(viewPosition, viewPosition + cameraFront, glm::vec3(0.0f, 1.0f, 0.0f));
 		
 		GLuint viewLocation = glGetUniformLocation(shader.program(), "view");
 		glUniformMatrix4fv(viewLocation, 1, GL_FALSE, glm::value_ptr(view));
@@ -133,8 +150,41 @@ int main(void) {
 }
 
 void handleInputs(GLFWwindow *window) {
-	if (glfwGetKey(window, GLFW_KEY_LEFT) || glfwGetKey(window, GLFW_KEY_A)) viewPosition.x += 0.1f;
-	if (glfwGetKey(window, GLFW_KEY_RIGHT) || glfwGetKey(window, GLFW_KEY_D)) viewPosition.x -= 0.1f;
-	if (glfwGetKey(window, GLFW_KEY_UP) || glfwGetKey(window, GLFW_KEY_W)) viewPosition.z += 0.1f;
-	if (glfwGetKey(window, GLFW_KEY_DOWN) || glfwGetKey(window, GLFW_KEY_S)) viewPosition.z -= 0.1f;
+	if (glfwGetKey(window, GLFW_KEY_LEFT) || glfwGetKey(window, GLFW_KEY_A)) 
+		viewPosition -= 0.1f * glm::normalize(glm::cross(cameraFront, cameraUp));
+	if (glfwGetKey(window, GLFW_KEY_RIGHT) || glfwGetKey(window, GLFW_KEY_D)) 
+		viewPosition += 0.1f * glm::normalize(glm::cross(cameraFront, cameraUp));
+	if (glfwGetKey(window, GLFW_KEY_UP) || glfwGetKey(window, GLFW_KEY_W)) 
+		viewPosition += 0.1f * cameraFront;
+	if (glfwGetKey(window, GLFW_KEY_DOWN) || glfwGetKey(window, GLFW_KEY_S)) 
+		viewPosition -= 0.1f * cameraFront;
+}
+
+void mouseCallback(GLFWwindow *window, GLdouble ax, GLdouble ay) {
+	float x = static_cast<GLfloat>(ax);
+	float y = static_cast<GLfloat>(ay);
+
+	GLfloat xoffset = x - lastX;
+	GLfloat yoffset = lastY - y;
+
+	lastX = x;
+	lastY = y;
+
+	xoffset *= sensitivity;
+	yoffset *= sensitivity;
+
+	yaw += xoffset;
+	pitch += yoffset;
+
+	direction.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
+	direction.y = sin(glm::radians(pitch));
+	direction.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
+
+	cameraFront = glm::normalize(direction);
+
+	if (pitch > 90.f) {
+		pitch = 90.f;
+	} else if (pitch < -90.f) {
+		pitch = -90.f;
+	}
 }
